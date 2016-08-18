bf2c6d0d245828ca50c1cfaace830e5bf6e2c447
Thomas Bandixen <thomas@bandixen.ch>
2016-08-18 15:25:50 +0200

    optimized the fetch routine

--- MailHeaders/Program.cs
+++ MailHeaders/Program.cs
@@ -25,42 +25,40 @@ namespace MailHeaders
             var port = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Amount (0 for all)");
             var amount = Convert.ToInt32(Console.ReadLine());
+#if DEBUG
+                amount = 5;
+#endif
 
-            FileStream filestream = new FileStream(DateTime.Now.ToString("yyMMddHHmmss") + "-Analyse-" + host + ".csv", FileMode.Create);
-            var streamwriter = new StreamWriter(filestream);
-            streamwriter.AutoFlush = true;
-            var output = Console.Out;
-            Console.SetOut(streamwriter);
+            using (FileStream filestream = new FileStream(DateTime.Now.ToLocalTime().ToString("yyMMddHHmmss") + "-Analyse-" + host + ".csv", FileMode.Create))
+            using (var streamwriter = new StreamWriter(filestream))
+            {
+                //streamwriter.AutoFlush = true;
+                //var output = Console.Out;
+                Console.SetOut(streamwriter);
 
-            var list = FetchAllMessages(username, password, host, useSsl, port, amount);
+                var list = FetchAllMessages(username, password, host, useSsl, port, amount);
 
-            Console.WriteLine("\"{0}\";\"{1}\";\"{2}\";\"{3}\";\"{4}\";\"{5}\"", "Date sent", "Subject", "Received date", "Delay", "Received by", "Received from");
-            list.ForEach(LogMessage);
+                Console.WriteLine($"\"{"Id"}\";\"{"Date sent"}\";\"{"Received date"}\";\"{"Delay"}\";\"{"Received by"}\";\"{"Received from"}\"");
+                list.OrderByDescending(i => i.DateSent).ToList().ForEach(LogMessage);
+            }
         }
 
         private static void LogMessage(MessageHeader header)
         {
-            DateTime lastDate = header.DateSent;
+            DateTime lastReceivedDate = header.DateSent.ToLocalTime();
             header.Received.OrderBy(r => r.Date).ToList().ForEach((r) =>
             {
-                TimeSpan delay = r.Date - lastDate;
-                if (delay < TimeSpan.FromSeconds(0))
-                {
-                    delay = TimeSpan.FromSeconds(0);
-                }
-                lastDate = r.Date;
+                var receivedDate = r.Date == DateTime.MinValue ? header.DateSent.ToLocalTime() : r.Date.ToLocalTime();
+                TimeSpan delay = receivedDate.Subtract(lastReceivedDate);
+                lastReceivedDate = receivedDate;
+                //if (delay < TimeSpan.FromSeconds(0))
+                //{
+                //    delay = TimeSpan.FromSeconds(0);
+                //}
                 var from = r.Names.Where(n => n.Key == "from").Select(n => n.Value).FirstOrDefault();
                 var by = r.Names.Where(n => n.Key == "by").Select(n => n.Value).FirstOrDefault();
-                WL((by ?? "").StartsWith("mail.vs-online.net") ? ConsoleColor.Red : ConsoleColor.White, "\"{0}\";\"{1}\";\"{2}\";\"{3}\";\"{4}\";\"{5}\"", header.DateSent, header.Subject, r.Date, delay, by, from);
+                Console.WriteLine($"\"{header.MessageId}\";\"{header.DateSent.ToLocalTime()}\";\"{receivedDate}\";\"{delay}\";\"{by}\";\"{from}\"");
             });
-        }
-
-        private static void WL(ConsoleColor color, string format, params object[] args)
-        {
-            var oldColor = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            Console.WriteLine(format, args);
-            Console.ForegroundColor = oldColor;
         }
 
         /// <summary>
@@ -73,12 +71,13 @@ namespace MailHeaders
         /// <param name="username">Username of the user on the server</param>
         /// <param name="password">Password of the user on the server</param>
         /// <returns>All Messages on the POP3 server</returns>
-        public static List<MessageHeader> FetchAllMessages(string username, string password, string hostname, bool useSsl = true, int port = 0, int amount = 0)
+        public static IEnumerable<MessageHeader> FetchAllMessages(string username, string password, string hostname, bool useSsl = true, int port = 0, int amount = 0)
         {
             if (port == 0)
             {
                 port = useSsl ? 995 : 110;
             }
+
             // The client disconnects from the server when being disposed
             using (Pop3Client client = new Pop3Client())
             {
@@ -91,21 +90,16 @@ namespace MailHeaders
                 // Get the number of messages in the inbox
                 int messageCount = client.GetMessageCount();
 
-                // We want to download all messages
-                List<MessageHeader> allMessages = new List<MessageHeader>(messageCount);
-
-                // Messages are numbered in the interval: [1, messageCount]
-                // Ergo: message numbers are 1-based.
-                // Most servers give the latest message the highest number
-
                 if (amount == 0)
                 {
                     amount = messageCount;
                 }
 
+                ICollection<MessageHeader> allMessages = new List<MessageHeader>(amount);
+
                 for (int i = messageCount; i > messageCount - amount; i--)
                 {
-                    var msg = client.GetMessageHeaders(i); // .GetMessage(i);
+                    var msg = client.GetMessageHeaders(i);
                     allMessages.Add(msg);
                 }
 
